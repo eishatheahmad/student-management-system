fd06e2898e47f3a6e08c63d1a701ae41027c93fb
Wasiq Malik <wasiq.malik13@gmail.com>
2021-06-20 17:48:27 +0500

    front/back merged

---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// create express app
+const app = express();
+
+// Setup server port
+const port = process.env.PORT || 5000;
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// define a root route
+app.get("/", (req, res) => {
+  res.send("Student Managment API V1.0");
+});
+
+// Require routes
+const studentRoutes = require("./src/routes/Student.route");
+const courseRoutes = require("./src/routes/Course.route");
+const studentCoursesRoutes = require("./src/routes/StudentCourse.route");
+
+// using as middleware
+app.use("/api/students", studentRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/studentCourses", studentCoursesRoutes);
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
